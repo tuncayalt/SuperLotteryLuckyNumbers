64e9ce9940d397162d26b9c97748728f75f01d43
tuncay <tuncayalt@gmail.com>
2017-07-15 19:45:58 +0300

    Without our

--- app/src/main/java/com/tuncay/superlotteryluckynumbers/MainActivity.java
+++ app/src/main/java/com/tuncay/superlotteryluckynumbers/MainActivity.java
@@ -1,35 +1,22 @@
 package com.tuncay.superlotteryluckynumbers;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.NumberPicker;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -37,19 +24,11 @@ import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
-import com.googlecode.tesseract.android.TessBaseAPI;
 import com.tuncay.superlotteryluckynumbers.adapter.CustomMainListAdapter;
 import com.tuncay.superlotteryluckynumbers.model.Coupon;
 import com.tuncay.superlotteryluckynumbers.model.MainListElement;
 import com.tuncay.superlotteryluckynumbers.service.IServerService;
-import com.tuncay.superlotteryluckynumbers.tools.BitmapUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -92,11 +71,6 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnVa
     Realm realm;
     ProgressDialog progress;
     InterstitialAd mInterstitialAd;
-    private static final int CAMERA_REQUEST = 1888;
-    private TessBaseAPI mTess; //Tess API reference
-    String datapath = ""; //path to folder containing language data file
-    Uri imageFileUri;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -141,19 +115,6 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnVa
 
         serverService = retrofit.create(IServerService.class);
 
-        datapath = getFilesDir() + "/tesseract/";
-        //make sure training data has been copied
-        checkFile(new File(datapath + "tessdata/"));
-        //initialize Tesseract API
-        String lang = "tur";
-        mTess = new TessBaseAPI();
-        mTess.init(datapath, lang);
-        mTess.setPageSegMode(TessBaseAPI.PageSegMode.PSM_AUTO_OSD);
-        mTess.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "1234567890");
-        mTess.setVariable(TessBaseAPI.VAR_CHAR_BLACKLIST, "!@#$%^&*()_+=-qwertyuiop[]}{POIU" +
-                "YTREWQasdASDfghFGHjklJKLl;L:'\"\\|~`xcvXCVbnmBNM,./<>?");
-
-
         GetDates task = new GetDates();
         task.execute();
         MobileAds.initialize(this, "ca-app-pub-5819132225601729~6536327892");
@@ -186,173 +147,6 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnVa
         }
         pickerMinNumber = 0;
         pickerViewNumber = 0;
-    }
-
-    public void KupondanAl(View view) {
-//        File file = new File(getFilesDir().getAbsolutePath() + "/kupon.jpg");
-//        Uri imageFileUri = Uri.fromFile(file);
-
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                Log.e("MainActivity", ex.getMessage());
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                imageFileUri = FileProvider.getUriForFile(this,
-                        "com.tuncay.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
-                startActivityForResult(takePictureIntent, CAMERA_REQUEST);
-            }
-        }
-    }
-
-    String mCurrentPhotoPath;
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String imageFileName = "kupon";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap image = null;
-            try {
-                //image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageFileUri);
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 4; // 1 - means max size. 4 - means maxsize/4 size. Don't use value <4, because you need more memory in the heap to store your data.
-                image = BitmapFactory.decodeFile(mCurrentPhotoPath, options);
-
-                image = modifyImage(image);
-                showKupondanAlDialog(image);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
-
-    private Bitmap modifyImage(Bitmap bitmap) throws IOException {
-
-        ExifInterface exif = new ExifInterface(imageFileUri.getPath());
-        int exifOrientation = exif.getAttributeInt(
-                ExifInterface.TAG_ORIENTATION,
-                ExifInterface.ORIENTATION_NORMAL);
-
-        int rotate = 0;
-
-        switch (exifOrientation) {
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                rotate = 90;
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                rotate = 180;
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                rotate = 270;
-                break;
-        }
-        rotate = 90;
-
-        if (rotate != 0) {
-
-            // Getting width & height of the given image.
-            int w = bitmap.getWidth();
-            int h = bitmap.getHeight();
-
-            // Setting pre rotate
-            Matrix mtx = new Matrix();
-            mtx.postRotate(rotate);
-
-            // Rotating Bitmap
-            bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
-        }
-
-        bitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth() * 2, (int)(bitmap.getHeight()), false);
-        bitmap = BitmapUtils.setGrayscale(bitmap);
-        bitmap = BitmapUtils.removeNoise(bitmap, 160);
-
-        return bitmap;
-
-    }
-
-    private void copyFiles() {
-        try {
-            //location we want the file to be at
-            String filepath = datapath + "/tessdata/tur.traineddata";
-
-            //get access to AssetManager
-            AssetManager assetManager = getAssets();
-
-            //open byte streams for reading/writing
-            InputStream instream = assetManager.open("tessdata/tur.traineddata");
-            OutputStream outstream = new FileOutputStream(filepath);
-
-            //copy the file to the location specified by filepath
-            byte[] buffer = new byte[1024];
-            int read;
-            while ((read = instream.read(buffer)) != -1) {
-                outstream.write(buffer, 0, read);
-            }
-            outstream.flush();
-            outstream.close();
-            instream.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void checkFile(File dir) {
-        //directory does not exist, but we can successfully create it
-        if (!dir.exists() && dir.mkdirs()) {
-            copyFiles();
-        }
-        //The directory exists, but there is no data file in it
-        if (dir.exists()) {
-            String datafilepath = datapath + "/tessdata/tur.traineddata";
-            File datafile = new File(datafilepath);
-            if (!datafile.exists()) {
-                copyFiles();
-            }
-        }
-    }
-
-    private void showKupondanAlDialog(Bitmap image) {
-        String OCRresult = null;
-        mTess.setImage(image);
-        OCRresult = mTess.getUTF8Text();
-        Log.d("MainActivity", "OCRresult:" + OCRresult);
-        mTess.end();
-
-        Dialog d = new Dialog(this);
-        d.setTitle("Numara düzelt");
-        d.setContentView(R.layout.kupondan_al_dialog);
-        d.setCanceledOnTouchOutside(false);
-        ImageView imgFoto = (ImageView) d.findViewById(R.id.imgFoto);
-        imgFoto.setImageBitmap(image);
-        d.show();
     }
 
     public void NumPicker(final View view) {
